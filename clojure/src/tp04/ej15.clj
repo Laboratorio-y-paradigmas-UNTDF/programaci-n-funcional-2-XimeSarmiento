4e4be68a55f5089541c94f12fecd8531cb86ca5e
xsarmiento <ximesarmiento2016@gmail.com>
2026-04-22 20:27:22 -0300

    clj ej 15

--- clojure/src/tp04/ej15.clj
+++ clojure/src/tp04/ej15.clj
@@ -4,14 +4,17 @@
 ;; Los primeros n pares positivos (2, 4, 6...).
 (defn primeros-n-pares [n]
   ;; TODO: implementar
+  (take n (iterate #(+ % 2) 2))
   )
 
 ;; Secuencia infinita de Fibonacci. DEBE ser lazy.
 (defn fibonacci []
   ;; TODO: implementar con lazy-seq o iterate
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1]))
   )
 
 ;; Toma elementos mientras sean menores que umbral.
 (defn tomar-mientras-menor [coll umbral]
   ;; TODO: implementar
+  (take-while #(< % umbral) coll)
   )
