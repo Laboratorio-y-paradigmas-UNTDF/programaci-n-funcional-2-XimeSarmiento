7c274a72e6f2796722a465b1a3933c45b670da01
xsarmiento <ximesarmiento2016@gmail.com>
2026-04-22 21:12:09 -0300

    clj ej 18

--- clojure/src/tp04/ej18.clj
+++ clojure/src/tp04/ej18.clj
@@ -4,15 +4,35 @@
 ;; {:ok true :value orden} si activa y total > 100. Error si no.
 (defn clasificar-orden [orden]
   ;; TODO: implementar
+  (cond
+    (not (:activa? orden)) {:ok false :error "orden inactiva"}
+    (<= (:total orden) 100) {:ok false :error "monto insuficiente"}
+    :else {:ok true :value orden})
   )
 
 ;; Retorna nueva orden con total reducido por porcentaje.
 (defn aplicar-descuento [porcentaje orden]
   ;; TODO: implementar
+  (update orden :total #(* % (- 1 (/ porcentaje 100))))
   )
 
 ;; Pipeline: clasificar → separar → descuento 10% → sumar.
 ;; Retorna {:aprobadas [...] :rechazadas [...] :total-final N}
 (defn procesar-ordenes [ordenes]
   ;; TODO: implementar
+  (let [clasificadas (map clasificar-orden ordenes)
+        aprobadas (->> clasificadas
+                       (filter :ok)
+                       (map :value)
+                       (map #(aplicar-descuento 10 %))
+                       (vec))
+        rechazadas (->> clasificadas
+                        (remove :ok)
+                        (vec))
+        total-final (->> aprobadas
+                         (map :total)
+                         (reduce + 0))]
+    {:aprobadas aprobadas
+     :rechazadas rechazadas
+     :total-final total-final})
   )
